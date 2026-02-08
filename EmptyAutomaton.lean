b81901fda31570e9bfd471b688eb88a8ca794418
Edward Banner <edward@Mac.lan>
2026-02-08 10:00:57 -0500

    Rename emptyAutomaton to M

--- EmptyAutomaton.lean
+++ EmptyAutomaton.lean
@@ -2,7 +2,7 @@ import Automaton
 
 set_option trace.Meta.Tactic.simp.rewrite true
 
-def emptyAutomaton : Automaton :=
+def M : Automaton :=
 {
   Q := Unit,
   «Σ» := Bool,
@@ -11,18 +11,18 @@ def emptyAutomaton : Automaton :=
   F := ∅
 }
 
-theorem emptyAutomaton_language_is_the_empty_set : L emptyAutomaton = ∅ := by
-  -- [Meta.Tactic.simp.rewrite] emptyAutomaton.eq_1:100:
-  --       emptyAutomaton
+theorem language_of_M_is_the_empty_set : L M = ∅ := by
+  -- [Meta.Tactic.simp.rewrite] M.eq_1:100:
+  --       M
   --     ==>
   --       { Q := Unit, «Σ» := Bool,
   --         δ := fun x ↦
   --           match x with
   --           | (PUnit.unit, snd) => (),
   --         q₀ := (), F := ∅ }
-  -- [Meta.Tactic.simp.rewrite] unfold L, L emptyAutomaton ==> {w | accepts emptyAutomaton w}
-  -- [Meta.Tactic.simp.rewrite] unfold accepts, accepts emptyAutomaton w ==> have q' := run emptyAutomaton w;
-  --     q' ∈ emptyAutomaton.F
+  -- [Meta.Tactic.simp.rewrite] unfold L, L M ==> {w | accepts M w}
+  -- [Meta.Tactic.simp.rewrite] unfold accepts, accepts M w ==> have q' := run M w;
+  --     q' ∈ M.F
   -- [Meta.Tactic.simp.rewrite] Set.mem_empty_iff_false:1000:
   --       run { Q := Unit, «Σ» := Bool, δ := fun x ↦ (), q₀ := (), F := ∅ } w ∈ ∅
   --     ==>
@@ -35,4 +35,4 @@ theorem emptyAutomaton_language_is_the_empty_set : L emptyAutomaton = ∅ := by
   --       ∅ = ∅
   --     ==>
   --       True
-  simp [L, accepts, emptyAutomaton]
+  simp [L, accepts, M]
