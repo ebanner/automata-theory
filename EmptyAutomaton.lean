b3f13fc4fb873be1253a4e85afce07c07897df81
Edward Banner <edward@Mac.lan>
2026-02-08 09:51:24 -0500

    Add simp output

--- EmptyAutomaton.lean
+++ EmptyAutomaton.lean
@@ -12,4 +12,27 @@ def emptyAutomaton : Automaton :=
 }
 
 theorem emptyAutomaton_language_is_the_empty_set : L emptyAutomaton = ∅ := by
+  -- [Meta.Tactic.simp.rewrite] emptyAutomaton.eq_1:100:
+  --       emptyAutomaton
+  --     ==>
+  --       { Q := Unit, «Σ» := Bool,
+  --         δ := fun x ↦
+  --           match x with
+  --           | (PUnit.unit, snd) => (),
+  --         q₀ := (), F := ∅ }
+  -- [Meta.Tactic.simp.rewrite] unfold L, L emptyAutomaton ==> {w | accepts emptyAutomaton w}
+  -- [Meta.Tactic.simp.rewrite] unfold accepts, accepts emptyAutomaton w ==> have q' := run emptyAutomaton w;
+  --     q' ∈ emptyAutomaton.F
+  -- [Meta.Tactic.simp.rewrite] Set.mem_empty_iff_false:1000:
+  --       run { Q := Unit, «Σ» := Bool, δ := fun x ↦ (), q₀ := (), F := ∅ } w ∈ ∅
+  --     ==>
+  --       False
+  -- [Meta.Tactic.simp.rewrite] Set.setOf_false:1000:
+  --       {w | False}
+  --     ==>
+  --       ∅
+  -- [Meta.Tactic.simp.rewrite] eq_self:1000:
+  --       ∅ = ∅
+  --     ==>
+  --       True
   simp [L, accepts, emptyAutomaton]
